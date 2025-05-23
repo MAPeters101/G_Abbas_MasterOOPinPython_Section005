b3bde310f28e7d47f1086a9d0e903331267ee270
Mark Peters <mapeters101@earthlink.net>
2025-05-23 12:32:03 -0400

    Work on lesson011 per author.

---
+++ lesson011.py
@@ -0,0 +1,35 @@
+class BMI:
+    def calculateBmi(self):
+        print("Enter your weight in KG: ")
+        self.weight = float(input())
+        print("Enter your height in meters: ")
+        self.height = float(input())
+
+        self.bmi = self.weight / (self.height * self.height)
+
+        print("Your BMI is: ", self.bmi)
+
+        if self.bmi < 18.5:
+            print("You are underweight.")
+        elif self.bmi > 18.5 and self.bmi < 24.9:
+            print("You are healthy.")
+        elif self.bmi > 24.9 and self.bmi < 29.9:
+            print("You are overweight.")
+        elif self.bmi > 30:
+            print("You are obese.")
+
+
+
+bmi = BMI()
+
+while True:
+    print("1. Calculate Your BMI")
+    print("2. Exit")
+
+    choice = int(input())
+
+    if choice is 1:
+        bmi.calculateBmi()
+    elif choice is 2:
+        exit()
+
